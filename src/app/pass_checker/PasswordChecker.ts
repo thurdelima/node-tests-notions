e57ca8b4b701acbbc024eda74a812f3273efd08d
Arthur Lima <thurdelima@gmail.com>
2023-07-24 21:31:16 -0300

    some tests with password

---
+++ src/app/pass_checker/PasswordChecker.ts
@@ -0,0 +1,22 @@
+
+export class PasswordChecker {
+
+
+
+    public checkPassword(password: string): boolean {
+
+        if (password.length < 8) {
+            return false;
+        }
+
+        if (password == password.toLowerCase()) {
+            return false;
+        }
+
+        if (password == password.toUpperCase()) {
+            return false;
+        }
+        return true;
+    }
+
+}
