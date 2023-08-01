ede5579d5a9d68ce7161cbf31b53c47fc3b25ba8
Arthur Lima <thurdelima@gmail.com>
2023-07-31 21:15:11 -0300

    some stubs

---
+++ src/app/doubles/OtherUtils.ts
@@ -0,0 +1,16 @@
+
+
+
+export type stringInfo = {
+    lowerCase: string,
+    upperCase: string,
+    characters: string[],
+    length: number,
+    extraInfo: Object 
+}
+
+
+
+export function calculateComplexity(stringInfo: stringInfo) {
+    return Object.keys(stringInfo.extraInfo).length * stringInfo.length;
+}
