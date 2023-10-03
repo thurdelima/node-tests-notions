09a395b94961c7113e5e9e52742ef4bfde2cad29
Arthur Lima <thurdelima@gmail.com>
2023-10-02 23:14:30 -0300

    some tests if no credentials in loginHandler

--- src/test/server_app2/LoginRequests.test.ts
+++ src/test/server_app2/LoginRequests.test.ts
@@ -3,6 +3,7 @@ import { ResponseTestWrapper } from "./test_utils/ResponseTestWrapper";
 import { HTTP_CODES, HTTP_METHODS } from "../../app/server_app/model/ServerModel";
 import { Account } from "../../app/server_app/model/AuthModel";
 import { DataBase } from "../../app/server_app/data/DataBase";
+import { Server } from '../../app/server_app/server/Server';
 
 
 jest.mock('../../app/server_app/data/DataBase');
@@ -49,6 +50,77 @@ describe('Login requests test suite', () => {
         requestWrapper.clearFields();
         responseWrapper.clearFields();
         jest.clearAllMocks();
+    })
+
+    it('should login user with valid credentials', async () => {
+        requestWrapper.method = HTTP_METHODS.POST;
+        requestWrapper.body = someAccount;
+        requestWrapper.url = 'localhost:8080/login';
+        getBySpy.mockResolvedValueOnce(someAccount);
+        insertSpy.mockResolvedValueOnce(someToken);
+
+        await new Server().startServer();
+
+        await new Promise(process.nextTick); //this solves timing issues
+
+        expect(responseWrapper.statusCode).toBe(HTTP_CODES.CREATED);
+        expect(responseWrapper.body).toEqual({
+            token: someToken
+        })
+        expect(responseWrapper.headers).toContainEqual(jsonHeader);
+
+
+    })
+
+    it('should login user with invalid credentials', async () => {
+        requestWrapper.method = HTTP_METHODS.POST;
+        requestWrapper.body = someAccount;
+        requestWrapper.url = 'localhost:8080/login';
+        getBySpy.mockResolvedValueOnce({
+            userName: 'someOtherUserName',
+            password: 'someOtherPassword'
+        });
+        
+
+        await new Server().startServer();
+
+        await new Promise(process.nextTick); //this solves timing issues
+
+        expect(responseWrapper.statusCode).toBe(HTTP_CODES.NOT_fOUND);
+       
+        expect(responseWrapper.body).toEqual('wrong username or password')
+
+
+    })
+
+    it('should return bad request if no credentials in request', async () => {
+        requestWrapper.method = HTTP_METHODS.POST;
+        requestWrapper.body = {};
+        requestWrapper.url = 'localhost:8080/login';
+
+        await new Server().startServer();
+
+        await new Promise(process.nextTick); //this solves timing issues
+
+        expect(responseWrapper.statusCode).toBe(HTTP_CODES.BAD_REQUEST);
+        expect(responseWrapper.headers).toContainEqual(jsonHeader);
+        expect(responseWrapper.body).toEqual('userName and password required');
+
+    })
+
+    it('should do nothing for not supported methods', async () => {
+        requestWrapper.method = HTTP_METHODS.DELETE;
+        requestWrapper.body = {};
+        requestWrapper.url = 'localhost:8080/login';
+
+        await new Server().startServer();
+
+        await new Promise(process.nextTick); // //this solves timing issues
+
+        expect(responseWrapper.statusCode).toBeUndefined();
+        expect(responseWrapper.headers).toHaveLength(0);
+        expect(responseWrapper.body).toBeUndefined();
+
     })
 
 
